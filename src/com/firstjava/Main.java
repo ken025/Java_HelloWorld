b5aa09ee1da2428cdb3b31b6d1ecbedd6b10e0c8
ken025 <kennethmanuel250@gmail.com>
2021-03-01 23:44:10 -0500

    ran first java program

--- src/com/firstjava/Main.java
+++ src/com/firstjava/Main.java
@@ -1,8 +1,12 @@
 package com.firstjava;
 
 public class Main {
-
+//    public = accessible throughout the program
+//    void = method won't return a value
     public static void main(String[] args) {
 	// write your code here
+
+//        Printing something
+        System.out.println("Hello World");
     }
 }
